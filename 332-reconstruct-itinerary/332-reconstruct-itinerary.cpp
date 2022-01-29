121b05e0c68b06eff68ca9a5b9f7652b28510c28
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-01-29 22:16:08 +0530

    Time: 24 ms (64.32%), Space: 15.2 MB (38.73%) - LeetHub

---
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    //using multiset instead of set because values maybe repeated. Also set/ multiset is sorted so it gets sorted in lexical order itself
+    unordered_map <string, multiset <string>> maptickets;       
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        for (vector <string> t: tickets)     //building graph
+            maptickets[t[0]].insert(t[1]); 
+        
+        vector <string> result;
+        dfs (result, "JFK");                 //starting from JFK
+        reverse (result.begin(), result.end());//result is created during return time of recursion thus reverse needed
+        return result;                       //returning result
+    }
+    
+    void dfs (vector <string> &result, string source) {
+        while (!maptickets[source].empty()) {
+            string dest = *maptickets[source].begin();     //declaring iterator for finding dest
+            maptickets[source].erase (maptickets[source].begin());  //deleting after travelling
+            dfs (result, dest);                                  
+        }
+        result.push_back(source);                                   //pushing into result
+    }    
+};
