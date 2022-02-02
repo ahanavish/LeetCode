47dcb8f5861ba44ecaa09e8afd53aed471ac52f7
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-02-03 00:32:41 +0530

    Time: 20 ms (58.43%), Space: 8.7 MB (76.26%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> count (26, 0), ret;
-        int diff = p.size();
-        for (auto ch : p) count[ch - 'a']++;
-        for (int i = 0; i < s.size(); i++) {
-            diff += count[s[i] - 'a']-- > 0 ? -1 : 1;
-            if (i >= p.size())
-                diff += count[s[i - p.size()] - 'a']++ >= 0 ? 1 : -1;
-            if (diff == 0) ret.push_back(i - p.size() + 1);
+        int ss=s.size(), ps=p.size();
+        if(ss<ps)           //since not possible
+            return {};
+        
+        vector<int> freq(26,0), window(26,0), ans;
+        
+        for(int i=0; i<ps; i++){    //first window
+            freq[p[i]-'a']++;       //using it to create our freq which will be used to compare with window later
+            window[s[i]-'a']++;
         }
-        return ret;
+        if(freq == window) 
+            ans.push_back(0);       //if first window itself is 
+
+        for(int i=ps; i<ss; i++){
+            window[s[i-ps]-'a']--;
+            window[s[i]-'a']++;
+
+            if(freq == window) 
+                ans.push_back(i-ps+1);
+        }
+        
+        return ans;
     }
 };
