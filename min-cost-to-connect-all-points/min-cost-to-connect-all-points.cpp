7860ad99b02df6681c1aa462158ead6a1f1720bf
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-02-07 18:38:30 +0530

    Time: 813 ms, Memory: 104.4 MB - LeetHub

--- min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,79 +1,48 @@
 class edges{
 public: 
-    int point1, point2, cost;       //declaring edges with 3 members
-    edges(int point1, int point2, int cost) : point1(point1), point2(point2), cost(cost){}
+    int point1, point2, cost;                   //declaring edges with 3 members
+    edges(int point1, int point2, int cost) : point1(point1), point2(point2), cost(cost){ }
 };
 
-bool operator<(const edges& edge1, const edges& edge2) {   // Overload the < operator for priority queue process
+bool operator< (const edges& edge1, const edges& edge2){      // Overload the < operator for priority queue process
     return edge1.cost > edge2.cost;
 }
-
-class unionfind{        //most optimised unionfind procedure
-private: 
-    vector <int> rank;      
-    vector <int> root;
-public:
-    unionfind(int size) : root(size), rank(size){
-        for(int i=0; i<size; i++){
-            rank[i] = 1;
-            root[i] = i;
-        }
-    }
-    
-    int find(int x){
-        if(x==root[x])
-            return x;
-        return root[x] = find(root[x]);
-    }
-    
-    void unionn(int x, int y){
-        int rootx=find(x), rooty=find(y);
-        if(rootx!=rooty){
-            if(rank[rootx]>rank[rooty])
-                root[rooty] = rootx;
-            else if(rank[rootx]<rank[rooty])
-                root[rootx] = rooty;
-            else{
-                root[rooty] = x;
-                rank[rootx]++;
-            }
-        }
-    }
-           
-    bool connected(int x, int y){
-       return find(x)==find(y);
-    }
-};
-           
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        if(points.size()==0)            //if it is empty, return 0;
+        if(points.size()==0)                //if it is empty, return 0;
             return 0;
+        
         int size=points.size();
-        unionfind uf(size);         
-        
+        vector <bool> vis(size, false);
         priority_queue <edges> pq;
-        for(int i=0; i<points.size(); i++)
-            for(int j=i+1; j<points.size(); j++){
-                int x1=points[i][0], x2=points[j][0], y1=points[i][1], y2=points[j][1];
-                int d=abs(x1-x2)+abs(y1-y2);
-                edges edge(i, j, d);    //creating edge with point1, point2, cost
-                pq.push(edge);          //putting edge in priority queue
-            }
-        
-        int result=0;
-        int edgeno=size-1;
-        while(!pq.empty() && edgeno>0){ //if any of these conditions fail ,we cant move forward
-            edges edge = pq.top();      
-            pq.pop();
-            if(!uf.connected(edge.point1, edge.point2)){
-                uf.unionn(edge.point1, edge.point2);        //if points are not connected yet, connect them
-                result += edge.cost;                        //thus adding it's cost in our result
-                edgeno--;                                   //and updating no of edges needed further
-            }            
+        for(int j=1; j<size; j++){
+            int x1=points[0][0], x2=points[j][0], y1=points[0][1], y2=points[j][1];
+            int d=abs(y2-y1)+abs(x2-x1);
+            edges edge(0, j, d);            //creating edge with point1, point2, cost
+            pq.push(edge);                  //putting edge in priority queue
         }
+        vis[0]=true;
         
+        int count=size-1;
+        int result=0;
+        while(!pq.empty() && count>0){      //if any of these conditions fail ,we cant move forward
+            edges edge = pq.top();
+            pq.pop();
+            if(!vis[edge.point2]){
+                vis[edge.point2]=true;
+                result += edge.cost;        //thus adding it's cost in our result
+                count--;                    //and updating no of edges needed further
+                for(int j=0; j<size; j++){  //putting new edges into the priority queue extending from point2
+                    if(j==edge.point2)
+                        continue;
+                    int x1=points[edge.point2][0], x2=points[j][0], y1=points[edge.point2][1], y2=points[j][1];
+                    int d=abs(y2-y1)+abs(x2-x1);
+                    edges edgee(edge.point2, j, d);
+                    pq.push(edgee);
+                }
+            }
+        }
         return result;
     }
 };
