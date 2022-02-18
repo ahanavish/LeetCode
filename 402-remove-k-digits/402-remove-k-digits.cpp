33e694dc8b7649886f179325045f5b928f2bf7c1
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-02-18 21:30:22 +0530

    Time: 0 ms (100.00%), Space: 7 MB (74.87%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-   string removeKdigits(string num, int k) {
-       string ans = "";                                         // treat ans as a stack in below for loop
-       
-       for (char c : num) {
-           while (ans.length() && ans.back() > c && k) {
-               ans.pop_back();                                  // make sure digits in ans are in ascending order
-               k--;                                             // remove one char
-           }
-           
-           if (ans.length() || c != '0') { ans.push_back(c); }  // can't have leading '0'
-       }
-       
-       while (ans.length() && k--) { ans.pop_back(); }          // make sure remove k digits in total
-       
-       return ans.empty() ? "0" : ans;
-}
+    string removeKdigits(string num, int k) {
+        string res="";
+        
+        for(auto c : num){
+            while(res.length() && res.back()>c && k){
+                res.pop_back();
+                k--;
+            }
+            
+            if(res.length() || c!='0')
+                res.push_back(c);
+        }
+        
+        while(res.length() && k){
+            k--;
+            res.pop_back();
+        }
+        
+        return res.empty()? "0" : res;
+    }
 };
