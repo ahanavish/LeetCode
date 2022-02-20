1bc4aa555557c44e9e3737daa25887d6ce839067
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-02-20 16:33:18 +0530

    Time: 489 ms (5.04%), Space: 11.4 MB (59.43%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -3,9 +3,14 @@ public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
         for(int i=0; i<intervals.size(); i++)
-            for(int j=0; j<intervals.size(); j++){
-                if(i!=j && intervals[i][0]>=intervals[j][0] && intervals[i][1]<=intervals[j][1]){
+            for(int j=i+1; j<intervals.size(); j++){
+                if(intervals[i][0]==intervals[j][0] && intervals[i][1]<=intervals[j][1]){
                     intervals.erase(intervals.begin()+i);
+                    i--;
+                    break;
+                }
+                else if(intervals[i][1]>=intervals[j][1]){
+                    intervals.erase(intervals.begin()+j);
                     i--;
                     break;
                 }
@@ -14,4 +19,3 @@ public:
         return intervals.size();
     }
 };
-    
