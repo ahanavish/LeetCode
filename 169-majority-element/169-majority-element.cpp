3fa3f174dca4e14b73194997899a716e18d793f0
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-02-21 22:48:25 +0530

    Time: 30 ms (41.73%), Space: 19.7 MB (9.42%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -5,10 +5,10 @@ public:
         for(auto n :nums)
             maxx[n]++;
         
-        int ans=INT_MIN,ele=0;
+        int ans=INT_MIN;
         for(auto n : maxx){
             if(nums.size()/2<n.second){
-                ans = max(ans, n.first);
+                ans = n.first;
             }
         }
         
