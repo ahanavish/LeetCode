7e949959dfcd89def098bc5fa30bf4015b8dea63
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-02-25 20:56:21 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (35.61%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    int compareVersion(string version1, string version2) {
-        int i=0,j=0,n=version1.length(),m=version2.length();
-        int num1=0,num2=0;
-        while(i<n || j<m){
-            while(i<n && version1[i]!='.' ){
-                num1=num1*10+(version1[i]-'0');
+    int compareVersion(string v1, string v2) {
+        int size1 = v1.length(), size2 = v2.length();
+        
+        int i=0, j=0, num1=0, num2=0;
+        while(i<size1 || j<size2){
+            while(i<size1 && v1[i]!='.'){
+                num1 = num1*10 + (v1[i]-'0');
                 i++;
-            }//end while
-            while(j<m && version2[j]!='.'){
-                num2=num2*10+(version2[j]-'0');
+            }
+            
+            while(j<size2 && v2[j]!='.'){
+                num2 = num2*10 + (v2[j]-'0');
                 j++;
-            }//end while
-            if(num1 > num2)
+            }
+            
+            if(num1>num2)
                 return 1;
-            else if(num1 < num2)
+            else if(num1<num2)
                 return -1;
-          num1=0;
-          num2=0;
-          i++;
-          j++;
-        }//end while
+            
+            i++;
+            j++;
+            num1=0;
+            num2=0;
+        }
+        
         return 0;
     }
 };
