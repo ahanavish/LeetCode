665d35284defbfd9ade992df7a9bfc6440d953f9
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-04-03 20:02:28 +0530

    Time: 67 ms (56.21%), Space: 24 MB (39.76%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     bool validPalindrome(string s) {
-        return valid(s, 0, s.length() - 1, 1);
+        int low=0, high=s.size()-1;
+        
+        while(low<high){
+            if(s[low]!=s[high])
+                return pal(s, low+1, high) || pal(s, low, high-1);
+            low++;
+            high--;
+        }
+        
+        return true;
     }
-
-private:
-    bool valid(string& s, int i, int j, int d) { // d: num of chars you can delete at most
-        if (i >= j) return true;
-        if (s[i] == s[j])
-            return valid(s, i + 1, j - 1, d);
-        else
-            return d > 0 && (valid(s, i + 1, j, d - 1) || valid(s, i, j - 1, d - 1));
+    
+    bool pal(string s, int low, int high){
+        while(low<high){
+            if(s[low]!=s[high])
+                return false;
+            low++;
+            high--;
+        }
+        
+        return true;
     }
 };
