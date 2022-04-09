531bda7539447cd951c6771fb7405f0cbf384eca
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-04-10 02:05:21 +0530

    Time: 344 ms (90.06%), Space: 173.2 MB (20.24%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    vector <int> nums, multi;
-    int m, n;
-    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
-        this->nums = nums;
-        multi = multipliers;
-        m = multipliers.size(), n = nums.size();     //exactly m operations
-        vector <vector<int>> mapp (m, vector<int>(m, 0));
+    int maximumScore(vector<int>& nums, vector<int>& multi) {
+        int m=multi.size(), n=nums.size();
         
-        return dp(0, 0, mapp);
+        vector <vector<int>> dp (m+1, vector<int> (m+1));
+        
+        for(int i=m-1; i>=0; i--){
+            for(int left=i; left>=0; left--){
+                int mult = multi[i], right = n-1-(i-left);
+                dp[i][left] = max(mult*nums[left]+dp[i+1][left+1], mult*nums[right]+dp[i+1][left]);
+            }
+        }
+        
+        return dp[0][0];
     }
-    
-    int dp(int i, int left, vector<vector<int>>& mapp){
-        if(i==m)
-            return 0;
-        
-        int mult = multi[i], right = n-1-i+left;
-        
-        if(mapp[i][left]==0)
-            mapp[i][left] = max(mult*nums[left]+dp(i+1, left+1, mapp), mult*nums[right]+dp(i+1, left, mapp));
-        
-        return mapp[i][left];
-    }
-}; 
+};
 
