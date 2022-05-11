ae9767432214b5ce846003fa7da60de3a7c111a4
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-05-12 01:18:55 +0530

    Time: 158 ms (19.54%), Space: 59 MB (39.25%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -5,17 +5,10 @@ public:
         int i=0, j=n-1, vol=0;
         
         while(i<j){
-            int shorth;
-            if(height[i]<height[j]){
-                shorth = height[i];
-                vol = max(vol, (j-i)*shorth);
-                i++;
-            }
-            else{
-                shorth = height[j];
-                vol = max(vol, (j-i)*shorth);
-                j--;
-            }
+            int shorth=min(height[i], height[j]);
+            vol = max(vol, (j-i)*shorth);
+            if(height[i]<height[j]) i++;
+            else j--;
         }
         return vol;
     }
