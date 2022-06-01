59707369276a694d1c44b021f5f79fbfe5af4d26
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-06-01 22:35:09 +0530

    Time: 10 ms (6.17%), Space: 6.3 MB (22.46%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
     int strStr(string hay, string need) {
-        int m=hay.size(), n=need.size();
+        int m=hay.length(), n=need.length();
+        if(!n)
+            return 0;
         
-        for(int i=0; i<=m-n; i++){
-            int j=0;
-            for(; j<n; j++)
-                if(hay[i+j] != need[j])
-                    break;
+        vector <int> kmp = kmpcompute(need);
+        for(int i=0, j=0; i<m;){
+            if(hay[i]==need[j])
+                i++, j++;
             if(j==n)
-                return i;
+                return i-j;
+            
+            if(i<m && hay[i]!=need[j])
+                j? j = kmp[j-1] : i++;
         }
         return -1;
+    }
+    
+    vector <int> kmpcompute(string need){
+        int n = need.length();
+        vector <int> kmp(n, 0);
+        
+        for(int i=1, j=0; i<n;){
+            if(need[i] == need[j])
+                kmp[i++] = ++j;
+            else if(j)
+                j = kmp[j-1];
+            else
+                kmp[i++] = 0;
+        }
+        return kmp;
     }
 };
