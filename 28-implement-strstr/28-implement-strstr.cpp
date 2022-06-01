68d9b376c738d14af19ebda34f7ac5744f821c3a
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-06-01 21:04:33 +0530

    Time: 2 ms (48.07%), Space: 6.3 MB (59.58%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     int strStr(string hay, string need) {
-        if(need.length()==0)
-            return 0;
+        int m=hay.size(), n=need.size();
         
-        int i=0, j=0, ch=0;
-        while(i<=hay.length()){
-            cout<<hay[i]<<need[j]<<" ";
-            if(ch==need.size())
-                return i-ch;
-            
-            if(ch!=0 && hay[i]!=need[j]){
-                i-=ch-1;
-                ch=0;
-                j=0;
-            }
-            
-            if(hay[i] == need[j]){
-                j++;
-                ch++;
-            }
-            
-            i++;
+        for(int i=0; i<=m-n; i++){
+            int j=0;
+            for(; j<n; j++)
+                if(hay[i+j] != need[j])
+                    break;
+            if(j==n)
+                return i;
         }
-        
         return -1;
     }
 };
