45479c9e1dc047737b9fa16df3471a7a1cf78820
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-06-02 10:50:56 +0530

    Time: 9 ms (62.96%), Space: 5.8 MB (92.26%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-    string intToRoman(int num) {
-        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
-        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        string ths[]={"","M","MM","MMM"};
-        
-        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+    const int val[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    const string rom[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+    string intToRoman(int N) {
+        string ans = "";
+        for (int i = 0; N; i++)
+            while (N >= val[i]) ans += rom[i], N -= val[i];
+        return ans;
     }
 };
