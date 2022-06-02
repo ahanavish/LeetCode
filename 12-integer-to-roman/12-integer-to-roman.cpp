2b014592de31d7e3db7c2a40efa15c385785fc81
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-06-02 14:41:50 +0530

    Time: 15 ms (39.89%), Space: 6 MB (78.77%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
-    const int val[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-    const string rom[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-
-    string intToRoman(int N) {
-        string ans = "";
-        for (int i = 0; N; i++)
-            while (N >= val[i]) ans += rom[i], N -= val[i];
+    string intToRoman(int num) {
+        int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string sym[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        string ans="";
+        int i=0;
+        while(num){
+            while(val[i]<=num){
+                num -= val[i];
+                ans += sym[i];
+            }
+            i++;
+        }
+        
         return ans;
     }
 };
