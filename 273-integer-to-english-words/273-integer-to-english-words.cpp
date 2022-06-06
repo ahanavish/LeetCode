b9d0102a9e7cfa540982745a8e501d4bb3f91f79
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-06-07 00:55:33 +0530

    Time: 11 ms (14.42%), Space: 7.3 MB (22.49%) - LeetHub

--- 273-integer-to-english-words/273-integer-to-english-words.cpp
+++ 273-integer-to-english-words/273-integer-to-english-words.cpp
@@ -1,32 +1,20 @@
 class Solution {
  public:
-    string digits[20] = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    string tens[10] = {"Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    vector<pair<int, string>> nums =
+{
+    {1000000000, "Billion"}, {1000000, "Million"}, {1000, "Thousand"}, {100, "Hundred"},
+    {90, "Ninety"},{80, "Eighty"},{70, "Seventy"},{60, "Sixty"}, 
+    {50, "Fifty"}, {40, "Forty"}, {30, "Thirty"}, {20, "Twenty"},
+    {19, "Nineteen"}, {18, "Eighteen"}, {17, "Seventeen"}, {16, "Sixteen"}, {15, "Fifteen"},
+    {14, "Fourteen"}, {13, "Thirteen"}, {12, "Twelve"}, {11, "Eleven"}, {10, "Ten"},
+    {9, "Nine"},{8, "Eight"},{7, "Seven"},{6, "Six"}, {5, "Five"}, {4, "Four"}, {3, "Three"}, {2, "Two"}, {1, "One"}
+};
 
-    string int2string(int n) {
-        if (n >= 1000000000) {
-            return int2string(n / 1000000000) + " Billion" + int2string(n % 1000000000);
-        } else if (n >= 1000000) {
-            return int2string(n / 1000000) + " Million" + int2string(n % 1000000);
-        } else if (n >= 1000) {
-            return int2string(n / 1000) + " Thousand" + int2string(n % 1000);
-        } else if (n >= 100) {
-            return int2string(n / 100) + " Hundred" + int2string(n % 100);
-        } else if (n >= 20) {
-            return  " " + tens[n / 10] + int2string(n % 10);
-        } else if (n >= 1) {
-            return " " + digits[n];
-        } else {
-            return "";
-        }
-    }
-
-    string numberToWords(int num) {
-        if (num == 0) {
-            return "Zero";
-        } else {
-            string ret = int2string(num);
-            return ret.substr(1, ret.length() - 1);
-        }
-    }
+string numberToWords(int num) {
+  if (num == 0) return "Zero";
+  for (auto it = nums.begin(); ; ++it)
+    if (num / it->first > 0)
+      return (num >= 100 ? numberToWords(num / it->first) + " " : "") + it->second +
+        (num % it->first == 0 ? "" : " " + numberToWords(num % it->first));
+}
 };
