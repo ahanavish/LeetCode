a3450706fb090074814862712b5df84c8120753a
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-06-08 15:22:08 +0530

    Time: 168 ms (5.04%), Space: 29.9 MB (30.73%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -3,10 +3,15 @@ public:
     int numDistinct(string s, string t) {
         int m = t.length(), n = s.length();
         vector<vector<double>> dp(m + 1, vector<double> (n + 1, 0));
-        for (int j = 0; j <= n; j++) dp[0][j] = 1;
+        for (int j = 0; j <= n; j++) 
+            dp[0][j] = 1;
         for (int j = 1; j <= n; j++)
             for (int i = 1; i <= m; i++)
-                dp[i][j] = dp[i][j - 1] + (t[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : 0);
+                if(t[i - 1] == s[j - 1])
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = dp[i][j - 1];
+        
         return (int)dp[m][n];
     }
 };  
