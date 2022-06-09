5c314d6b7193276e8d4070976fbbb6d42604c948
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-06-10 00:48:58 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (55.89%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int i=a.length()-1, j=b.length()-1, c=0;
+        reverse(a.begin(),a.end());
+        reverse(b.begin(),b.end());
+        int carry = 0;
         string ans="";
         
-        while(i>=0 || j>=0 || c==1){
-            c += i>=0? a[i--] - '0' : 0;
-            c += j>=0? b[j--] - '0' : 0;
-            ans = char(c%2 + '0') + ans;
-            c /= 2;
+        int i=0;
+        while(i<a.length() && i<b.length()){
+            int sum = carry + (a[i] - '0') + (b[i]-'0');
+            carry = sum>=2 ? 1 : 0;
+            ans += (sum%2) + '0';
+            i++;
         }
         
+
+        while(i<a.length()){
+            int sum = carry + (a[i] - '0');
+            carry = sum>=2 ? 1 : 0;
+            ans += (sum%2) + '0';
+            i++;
+        }
+        
+        while(i<b.length()){
+            int sum = carry + (b[i] - '0');
+            carry = sum>=2 ? 1 : 0;
+            ans += (sum%2) + '0';
+            i++;
+        }
+        
+        if(carry) ans += carry + '0';
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
