3b3320b14df53ee34bfa0420135957a69aad9b37
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-07-19 23:37:22 +0530

    Time: 0 ms (100.00%), Space: 6 MB (92.26%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -4,22 +4,23 @@ public:
         if(s.empty())
             return 0;
         
-        vector<int> dp(s.length()+1, 0);
-        dp[s.length()] = 1; 
+        int next2=0, next1=1, curr=0;
             
         for(int ind=s.length()-1; ind>=0; ind--){
             if(s[ind] == '0')
-                dp[ind] = 0;
+                curr = 0;
             else{
-                int one_char = dp[ind+1];
+                int one_char = next1;
                 int two_char = 0;
                 if(ind<s.length()-1 && (s[ind]=='1'|| (s[ind]=='2' && s[ind+1]<'7'))) 
-                    two_char = dp[ind+2];
+                    two_char = next2;
 
-                dp[ind] = one_char+two_char;
-            }
+                curr = one_char+two_char;
+            } 
+            next2 = next1;
+            next1 = curr;
         }
         
-        return dp[0];    
+        return curr;    
     }
 };
