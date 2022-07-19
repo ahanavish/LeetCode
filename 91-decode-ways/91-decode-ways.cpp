38e19e370c82429d03a7a45ba95054fdb8425ed0
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-07-19 23:32:16 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (46.49%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -4,24 +4,22 @@ public:
         if(s.empty())
             return 0;
         
-        vector<int> dp(s.length(), -1);
-        return numDecodings(0, s, dp);    
-    }
-    
-    int numDecodings(int ind, string& s, vector<int> &dp) {
-        if(ind == s.length()) 
-            return 1;
-        if(s[ind] == '0') 
-            return 0;   // sub string starting with 0 is not a valid encoding
+        vector<int> dp(s.length()+1, 0);
+        dp[s.length()] = 1; 
+            
+        for(int ind=s.length()-1; ind>=0; ind--){
+            if(s[ind] == '0')
+                dp[ind] = 0;
+            else{
+                int one_char = dp[ind+1];
+                int two_char = 0;
+                if(ind<s.length()-1 && (s[ind]=='1'|| (s[ind]=='2' && s[ind+1]<'7'))) 
+                    two_char = dp[ind+2];
+
+                dp[ind] = one_char+two_char;
+            }
+        }
         
-        if(dp[ind] != -1)
-            return dp[ind];
-        
-        int one_char = numDecodings(ind+1, s, dp);
-        int two_char = 0;
-        if(ind<s.length()-1 && (s[ind]=='1'|| (s[ind]=='2' && s[ind+1]<'7'))) 
-            two_char = numDecodings(ind+2, s, dp);
-        
-        return dp[ind] = one_char + two_char;
+        return dp[0];    
     }
 };
