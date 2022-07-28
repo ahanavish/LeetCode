6793a751c54c1eda8179635bebee6ff0308e1084
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-07-28 16:01:26 +0530

    Time: 6 ms (59.52%), Space: 13 MB (63.78%) - LeetHub

---
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector <string> ans;
+        func(to_string(root->val), root, ans);
+        return ans;
+    }
+    
+    void func(string s, TreeNode* root, vector<string> &ans){ 
+        if(root->left)
+            func(s+"->"+to_string(root->left->val), root->left, ans);
+        if(root->right)
+            func(s+"->"+to_string(root->right->val), root->right, ans);
+        if(!root->left && !root->right)
+            ans.push_back(s);
+    }
+};
