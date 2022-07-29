d50ecdf896d217709756eda45eaa151bdf1483bc
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-07-29 18:16:16 +0530

    Time: 84 ms (13.30%), Space: 32.2 MB (98.29%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -9,35 +9,26 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if(!root1)
-            return root2;
-        if(!root2)
-            return root1;
+        if(!root1)  return root2;
+        if(!root2)  return root1;
         
         root1->val += root2->val;
         
-        if(!root1->left)    root1->left = root2->left;
-        else    func(root1->left, root2->left);
+        if(!root1->left)    
+            root1->left = root2->left;
+        else    
+            mergeTrees(root1->left, root2->left);
         
-        if(!root1->right)   root1->right = root2->right;
-        else    func(root1->right, root2->right);
+        if(!root1->right)   
+            root1->right = root2->right;
+        else    
+            mergeTrees(root1->right, root2->right);
         
         return root1;
     }
-    
-    void func(TreeNode* root1, TreeNode* root2){
-        if(!root1)  return;
-        if(!root2)  return;
-        
-        root1->val += root2->val;
-        
-        if(!root1->left)    root1->left = root2->left;
-        else    func(root1->left, root2->left);
-        
-        if(!root1->right)   root1->right = root2->right;
-        else    func(root1->right, root2->right);
-    }
 };
+
