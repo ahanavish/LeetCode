691a5e6e13c01a720d626e08f4a8fdfdd1e78c41
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-08-05 19:38:22 +0530

    Time: 3 ms (62.90%), Space: 6.7 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target+1, -1);
+        vector< unsigned int> dp(target+1);
         dp[0]=1;
         
-        func(nums, target, dp);
+        for(int i=1; i<target+1; i++)
+            for(int j=0; j<nums.size(); j++)
+                if(nums[j]<=i)
+                    dp[i] += dp[i-nums[j]];
+        
         return dp[target];
     }
-    
-    int func(vector<int> &nums, int target, vector<int> &dp){
-        if(target<0)
-            return 0;
-    
-        if(dp[target] != -1)
-            return dp[target];
-        
-        int res=0;
-        for(int i=0; i<nums.size(); i++)
-            res+= func(nums, target-nums[i], dp);
-        
-        return dp[target] = res;
-    }
 };
+
