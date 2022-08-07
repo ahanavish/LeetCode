4bd3b16ec7aa7196e4781d71f8af65fab3cfa2d9
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-08-07 14:07:19 +0530

    Time: 19 ms (75.86%), Space: 17.5 MB (50.47%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -2,38 +2,24 @@ class Solution {
 public:
     int mod = 1e9+7;
     int countVowelPermutation(int n) {
-        if(n==1)
-            return 5;
-        
-        vector<vector<int>> dp(5, vector<int> (n+1, -1));
+        vector<int> vowels({'a', 'e', 'i', 'o', 'u'});
+        vector<vector<long>> dp(5, vector<long> (n+1, 0));
         for(int i=0; i<5; i++)
             dp[i][n]=1;
         
-        vector<int> vowels({'a', 'e', 'i', 'o', 'u'});
-        int c=0;
+      
+        for(int len=n-1; len>0; len--){
+            dp[0][len]=  (dp[1][len+1])%mod;
+            dp[1][len] = (dp[0][len+1]+dp[2][len+1])%mod;
+            dp[2][len] = (dp[0][len+1]+dp[1][len+1]+dp[3][len+1]+dp[4][len+1])%mod;
+            dp[3][len] = (dp[2][len+1]+dp[4][len+1])%mod;
+            dp[4][len] = (dp[0][len+1])%mod;
+        }
+        
+        int ans=0;
         for(int i=0; i<5; i++)
-            c = (c+count(i, 1, n, vowels, dp))%mod;
-            
-        return c;
-    }
-    
-    long long int count(int prev, int len, int n, vector<int> vowels, vector<vector<int>> &dp){
-        if(dp[prev][len] != -1)
-            return dp[prev][len]%mod;
+            ans = (ans+dp[i][1])%mod;
         
-        long long int c=0;
-        if(prev == 0)
-            c = count(1, len+1, n, vowels, dp)%mod;
-        else if(prev == 1)
-            c = (count(0, len+1, n, vowels, dp)+count(2, len+1, n, vowels, dp))%mod;
-        else if(prev == 2)
-            c = (count(0, len+1, n, vowels, dp)+count(1, len+1, n, vowels, dp)+
-                    count(3, len+1, n, vowels, dp)+count(4, len+1, n, vowels, dp))%mod;
-        else if(prev == 3)
-            c = (count(2, len+1, n, vowels, dp)+count(4, len+1, n, vowels, dp))%mod;
-        else if(prev == 4)
-            c = (count(0, len+1, n, vowels, dp))%mod;
-        
-        return dp[prev][len] = c;
+        return ans;
     }
 };
