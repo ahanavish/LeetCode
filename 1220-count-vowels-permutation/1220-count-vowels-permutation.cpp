c71ea7b4a1cb5d5ee97a9178016082a49c017c5c
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-08-07 14:16:04 +0530

    Time: 44 ms (62.15%), Space: 6 MB (90.50%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -3,22 +3,22 @@ public:
     int mod = 1e9+7;
     int countVowelPermutation(int n) {
         vector<int> vowels({'a', 'e', 'i', 'o', 'u'});
-        vector<vector<long>> dp(5, vector<long> (n+1, 0));
-        for(int i=0; i<5; i++)
-            dp[i][n]=1;
-        
+        vector<long> prev(5, 1);
       
+        vector<long> curr(5, 0);
         for(int len=n-1; len>0; len--){
-            dp[0][len]=  (dp[1][len+1])%mod;
-            dp[1][len] = (dp[0][len+1]+dp[2][len+1])%mod;
-            dp[2][len] = (dp[0][len+1]+dp[1][len+1]+dp[3][len+1]+dp[4][len+1])%mod;
-            dp[3][len] = (dp[2][len+1]+dp[4][len+1])%mod;
-            dp[4][len] = (dp[0][len+1])%mod;
+            curr[0] = prev[1]%mod;
+            curr[1] = (prev[0]+prev[2])%mod;
+            curr[2] = (prev[0]+prev[1]+prev[3]+prev[4])%mod;
+            curr[3] = (prev[2]+prev[4])%mod;
+            curr[4] = (prev[0])%mod;
+            
+            prev = curr;
         }
         
         int ans=0;
         for(int i=0; i<5; i++)
-            ans = (ans+dp[i][1])%mod;
+            ans = (ans+prev[i])%mod;
         
         return ans;
     }
