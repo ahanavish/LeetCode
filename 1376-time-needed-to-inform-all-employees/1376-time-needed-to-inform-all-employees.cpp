77ab909621b2cbdf8fb0f4906307e65f6d2fe548
Ahana Vishwakarma <64797076+ahanavish@users.noreply.github.com>
2022-08-08 15:55:33 +0530

    Time: 367 ms (75.92%), Space: 122.2 MB (49.60%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numOfMinutes(int n, int head, vector<int>& manager, vector<int>& inform) {
+        if(n==1)
+            return 0;
+        
+        vector<vector<int>> adj(n);
+        for(int i=0; i<n; i++)
+            if(i!=head)
+                adj[manager[i]].push_back(i);
+        
+        return dfs(head, adj, manager, inform);
+    }
+    
+    int dfs(int i, vector<vector<int>> &adj, vector<int>& manager, vector<int>& inform){
+        int count=0;
+        for(auto j :adj[i])
+                count = max(count, dfs(j, adj, manager, inform));
+        
+        return count+inform[i];
+    }
+};
